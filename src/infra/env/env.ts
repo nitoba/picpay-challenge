efaf3f58db2d2f6d6224c9429aba1a5b34686fad
Bruno Alves <nito.dev@gmail.com>
2024-03-24 13:42:11 -0300

    chore: setup rabbitmq config module

--- src/infra/env/env.ts
+++ src/infra/env/env.ts
@@ -3,6 +3,7 @@ import { z } from 'zod'
 export const envSchema = z.object({
   PORT: z.number().default(3333),
   DATABASE_URL: z.string().url(),
+  RABBIT_MQ_URI: z.string().url(),
   PIC_AUTHORIZE_TRANSACTION_GATEWAY_URL: z.string().url(),
   NOTIFICATION_SERVICE_URL: z.string().url(),
 })
--- src/infra/messaging/messaging.module.ts
+++ src/infra/messaging/messaging.module.ts
@@ -1,14 +1,52 @@
 import { PubSubMessaging } from '@/domain/notification/application/messaging/pubsub-messaging'
 import { Module } from '@nestjs/common'
 import { RabbitMqPubSubMessaging } from './pubsub/rabbitmq/rabbitmq-pubsub-messaging'
+import { NotificationSender } from '@/domain/notification/application/messaging/notification-sender'
+import { EmailNotificationService } from './notification/notification.service'
+import { SendEmailNotificationAboutTransactionUseCase } from '@/domain/notification/application/usecases/send-email-notification-about-transaction'
+import { DatabaseModule } from '../database/database.module'
+import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq'
+import { EnvModule } from '../env/env.module'
+import { EnvService } from '../env/env.service'
 
 @Module({
+  imports: [
+    DatabaseModule,
+    RabbitMQModule.forRootAsync(RabbitMQModule, {
+      imports: [EnvModule],
+      inject: [EnvService],
+      useFactory: async (env: EnvService) => ({
+        uri: env.get('RABBIT_MQ_URI'),
+        queues: [
+          {
+            name: 'transaction-created',
+            exchange: 'transactions',
+            routingKey: 'transaction-created',
+            createQueueIfNotExists: true,
+          },
+        ],
+        exchanges: [
+          {
+            name: 'transactions',
+            type: 'direct',
+            createExchangeIfNotExists: true,
+          },
+        ],
+        connectionInitOptions: { wait: true },
+      }),
+    }),
+  ],
   providers: [
     {
       provide: PubSubMessaging,
       useClass: RabbitMqPubSubMessaging,
     },
+    {
+      provide: NotificationSender,
+      useClass: EmailNotificationService,
+    },
+    SendEmailNotificationAboutTransactionUseCase,
   ],
-  exports: [PubSubMessaging],
+  exports: [PubSubMessaging, NotificationSender],
 })
 export class MessagingModule {}
